1ebc7db576b99bb39755a79c0da63035634aeddb
AM77 <amine.griche77@gmail.com>
2019-09-10 20:19:54 +0200

    added service and thiers owl-carousel slider

--- Amir/assets/js/script.js
+++ Amir/assets/js/script.js
@@ -13,4 +13,29 @@
         })
     }
     fix_navbar()
+
+    //* Setting the services slider
+    function services_slider() {
+        $('.services .owl-carousel').owlCarousel({
+            loop: true,
+            margin: 30,
+            items: 3,
+            autoplay: false,
+            smartSpeed: 2500,
+            dots: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                991: {
+                    items: 2
+                },
+                1200: {
+                    items: 4
+                }
+            }
+        })
+    }
+    services_slider();
 })(jQuery)
